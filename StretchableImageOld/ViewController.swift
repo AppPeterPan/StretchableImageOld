1126a98544961e3b34b5b4849bf192c50a9590be
peterpan <apppeterpan@gmail.com>
2019-08-18 14:00:18 +0800

    Initial Commit

---
+++ StretchableImageOld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StretchableImageOld
+//
+//  Created by SHIH-YING PAN on 2019/8/18.
+//  Copyright © 2019 SHIH-YING PAN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
